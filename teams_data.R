7162b7bcffa4902f28b2bcdf0c7664f4aa886b08
blp92 <blp92@users.noreply.github.com>
2017-06-13 15:00:29 -0400

    Create teams_data.R

--- teams_data.R
+++ teams_data.R
@@ -18,13 +18,13 @@ teams = tbl_df(data.frame(teams_data$resultSets$rowSet[[1]], stringsAsFactors = 
 names(teams) = tolower(teams_data$resultSets$headers[[1]])
 
 teams = mutate(teams,
-  from_year = as.numeric(from_year),
-  to_year = as.numeric(to_year),
+  min_year = as.numeric(min_year),
+  max_year = as.numeric(max_year),
   team_id = as.numeric(team_id)
 )
 
 if (Sys.Date() <= as.Date("2016-10-25")) {
-  teams = mutate(teams, to_year = pmin(to_year, 2015))
+  teams = mutate(teams, max_year = pmin(max_year, 2015))
 }
 
 #teams$name = sapply(team$display_last_comma_first, function(s) {
@@ -32,38 +32,40 @@ if (Sys.Date() <= as.Date("2016-10-25")) {
 #})
 
 first_year_of_data = 1996
-last_year_of_data = max(teams$to_year)
+last_year_of_data = max(teams$max_year)
 season_strings = paste(first_year_of_data:last_year_of_data,
                        substr(first_year_of_data:last_year_of_data + 1, 3, 4),
                        sep = "-")
 names(season_strings) = first_year_of_data:last_year_of_data
 
-available_teams = filter(teams, to_year >= first_year_of_data)
+available_teams = filter(teams, max_year >= first_year_of_data)
 
-names_table = table(available_players$name)
-dupe_names = names(names_table[which(names_table > 1)])
+team_names_table = table(available_teams$abbreviation)
+dupe_team_names = names(team_names_table[which(team_names_table > 1)])
 
-available_players$name[available_players$name %in% dupe_names] = paste(
-  available_players$name[available_players$name %in% dupe_names],
-  available_players$person_id[available_players$name %in% dupe_names]
+#could comment this all out because team_names_table is already only the 30 unique current teams
+available_teams$abbreviation[available_teams$abbreviation %in% dupe_team_names] = paste(
+  available_teams$abbreviation[available_teams$abbreviation %in% dupe_team_names],
+  available_teams$team_id[available_teams$abbreviation %in% dupe_team_names]
 )
 
-available_players$lower_name = tolower(available_players$name)
-available_players = arrange(available_players, lower_name)
+available_teams$lower_name = tolower(available_teams$abbreviation)
+available_teams = arrange(available_teams, lower_name)
 
-find_player_by_name = function(n) {
-  filter(available_players, lower_name == tolower(n))
+find_team_by_name = function(n) {
+  filter(available_teams, lower_name == tolower(n))
 }
 
-find_player_id_by_name = function(n) {
-  find_player_by_name(n)$person_id
+find_teamr_id_by_name = function(n) {
+  find_team_by_name(n)$team_id
 }
 
-default_player = find_player_by_name("Stephen Curry")
-default_years = as.character(default_player$from_year:default_player$to_year)
+default_team = find_team_by_name("MIN")
+default_years = as.character(default_team$min_year:default_player$max_year)
 default_seasons = as.character(season_strings[default_years])
 default_season = rev(default_seasons)[1]
 
-player_photo_url = function(player_id) {
-  paste0("http://stats.nba.com/media/players/230x185/", player_id, ".png")
-}
+#--------Can't find the URL to get team logos easily
+#team_photo_url = function(player_id) {
+#  paste0("http://stats.nba.com/media/players/230x185/", player_id, ".png")
+#}
