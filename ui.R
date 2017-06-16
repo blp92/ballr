5f0f3600aac75dc52f3aea8a095e99a8c2320ab8
blp92 <blp92@users.noreply.github.com>
2017-06-15 22:21:59 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -87,7 +87,7 @@ shinyUI(
             
           selectInput(inputId = "player_name",
                       label = "Player2",
-                      choices = c("Enter a player..." = "", available_players$name),
+                      choices = c("Enter a player..." = "", c("--none--", available_players$name),
                       selected = default_player2$name,
                       selectize = FALSE),
 
